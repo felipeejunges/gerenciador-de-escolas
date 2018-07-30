3aae201237ef9043042e2411ea0b81aa43aac700
Fernando M. Dufour <fm.dufour@gmail.com>
2018-07-30 14:38:23 -0300

    boilerplate projeto

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# gerenciador-de-escolas
+![Logo BRG](https://image.ibb.co/isVc3o/image.png)
 
-Sistema administrativo para Escolas.
+# Processo Seletivo - Itinerários do Ensino Médio
+
+Hey, ficamos muito felizes por seu interesse em participar de nosso processo seletivo!
+
+Nesta etapa do processo queremos testar sua capacidade de implementar um sistema de CRUD (Create, Read, Update e Delete).
+
+
+## Sistemas Gerenciador de Escolas
+
+Seu objetivo é desenvolver um sistema para gerenciamento de Escolas, Turmas e Alunos. O projeto deve ser na linguagem C# utilizando o ASP.NET Core 2.0 e o padrão MVC.
+
+Você deve copiar o template de projeto presente neste repositório e atender aos seguintes requisitos:
+  - Criar e utilizar um banco de dados local Sqlite3;
+  - Utilizar EntityFrameworkCore 2.0 para acesso aos dados;
+  - Criar as tabelas de Escola, Turma e Alunos.
+  - Implementar telas de cadastro, consulta, edição e uma função para deleção dos registros de cada uma.
+
+##### O que será avaliado no seu projeto:
+  - Atendimento dos requisitos;
+  - Organização de código;
+  - Design das telas.
---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace gerenciador_de_escolas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {            
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
