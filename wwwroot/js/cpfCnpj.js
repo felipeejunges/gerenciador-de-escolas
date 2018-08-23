eee6747f66e87f20dd9002112e1ab838b3e8c335
felipeejunges <felipeejunges@hotmail.com>
2018-08-23 13:57:27 -0300

    Adicionado os JavaScripts

---
+++ wwwroot/js/cpfCnpj.js
@@ -0,0 +1,18 @@
+﻿formataCnpjCpf();
+$(document).ready(function () { formataCnpjCpf(); });
+
+function formataCnpjCpf() {
+    var SPMaskBehavior = function (val) {
+        return val.replace(/\D/g, '').length > 11 ? '00.000.000/0000-00' : '000.000.000-009';
+    },
+        spOptions = {
+            onKeyPress: function (val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+    $('.cnpjcpf').mask(SPMaskBehavior, spOptions);
+    $('.cnpjcpf').mask(SPMaskBehavior, spOptions);
+    $('.cpf').mask('000.000.000-00', { reverse: true });
+    $('.cnpj').mask('00.000.000/0000-00', { reverse: true });
+}
+
---
+++ wwwroot/js/moeda.js
@@ -0,0 +1,43 @@
+﻿function carregaMaskMoney() {
+    $('.moeda').maskMoney({ prefix: 'R$ ', allowNegative: true, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.moeda').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+    console.log("Tipo: " + typeof (valor) + ", Valor: " + valor)
+
+    $('.moedasemnegativo').maskMoney({ prefix: 'R$ ', allowNegative: false, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.moedasemnegativo').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+    console.log("Tipo: " + typeof (valor) + ", Valor: " + valor)
+
+    $('.porcentagem').maskMoney({ suffix: '%', allowNegative: false, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.porcentagem').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+
+    $('.numeral').maskMoney({allowNegative: false, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.numeral').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+
+    $('.centimetro').maskMoney({ suffix: ' cm', allowNegative: false, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.centimetro').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+
+    $('.milimitro').maskMoney({ suffix: ' mm', allowNegative: false, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.milimitro').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+
+    $('.metro').maskMoney({ suffix: ' m', allowNegative: false, thousands: ".", decimal: ",", affixesStay: true, allowZero: true });
+    var valor = $('.metro').each(function () {
+        $(this).maskMoney('mask', $(this).val())[0].value;
+    });
+
+}
+carregaMaskMoney();
+$(document).ready(function () {
+    carregaMaskMoney();
+});
